cccb0bb0bc4b38466f4a8b077ff41b495b74deca
Marc Delagrammatikas <delagrammatikas@gmail.com>
2018-12-19 05:36:42 -0800

    Get secret key base from env
    Set with `cf set-env pcf_phoenix_spike SECRET_KEY_BASE "the_key"`

--- config/prod.exs
+++ config/prod.exs
@@ -68,4 +68,9 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-import_config "prod.secret.exs"
+#import_config "prod.secret.exs"
+
+secret_key_base = System.get_env("SECRET_KEY_BASE") || raise "SECRET_KEY_BASE must be set"
+
+config :pcf_phoenix_spike, PcfPhoenixSpikeWeb.Endpoint,
+       secret_key_base: secret_key_base
