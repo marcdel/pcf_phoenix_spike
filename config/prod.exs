e7198c145438467b5bd10143ae12e47c2f2869b2
Marc Delagrammatikas <delagrammatikas@gmail.com>
2018-12-19 05:37:20 -0800

    This appears to fix an issue with sockets (eafnosupport endpoint error)

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :pcf_phoenix_spike, PcfPhoenixSpikeWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
+  http: [:inet4, port: System.get_env("PORT") || 4000],
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
